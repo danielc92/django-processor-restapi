b137b8f52cab64b3886f1d3514c5f09283a77662
Daniel Corcoran <danielc.92@outlook.com>
2019-06-10 10:47:30 +1000

    Registered models

--- processors/admin.py
+++ processors/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Processors, Manufacturer, ProductSeries
 
 # Register your models here.
+admin.site.register(Processors)
+admin.site.register(Manufacturer)
+admin.site.register(ProductSeries)
