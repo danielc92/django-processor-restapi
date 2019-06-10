53c297c0487f0326d546c9497cb642d0ad609246
Daniel Corcoran <danielc.92@outlook.com>
2019-06-10 11:11:46 +1000

    Added remaining two views

--- processors/views.py
+++ processors/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .models import Processors
-from .serializers import ProcessorSerializer
+from .models import Processors, Manufacturer, ProductSeries
+from .serializers import ProcessorSerializer, ManufacturerSerializer, ProductSeriesSerializer
+
 
 class ProcessorView(viewsets.ModelViewSet):
     queryset = Processors.objects.all()
     serializer_class = ProcessorSerializer 
+
+
+class ManufacturerView(viewsets.ModelViewSet):
+    queryset = Manufacturer.objects.all()
+    serializer_class = ManufacturerSerializer 
+
+
+class ProductSeriesView(viewsets.ModelViewSet):
+    queryset = ProductSeries.objects.all()
+    serializer_class = ProductSeriesSerializer 
