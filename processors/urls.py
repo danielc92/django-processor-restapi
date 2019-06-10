81bab5f697f5876041f63411eb757d1dfbd0f5de
Daniel Corcoran <danielc.92@outlook.com>
2019-06-10 11:02:44 +1000

    Added urls with rest_framework router

--- processors/urls.py
+++ processors/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path, include
+from . import views
+from rest_framework import routers
+
+
+router = routers.DefaultRouter()
+router.register('processors', views.ProcessorView)
+
 
 urlpatterns = [
-    
-
+    path('', include(router.urls))
 ]
