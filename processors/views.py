ef1dbbbb3e13de71e6171d9fa60adc78f3490344
Daniel Corcoran <danielc.92@outlook.com>
2019-06-10 11:00:41 +1000

    Added a viewset for processors

--- processors/views.py
+++ processors/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Processors
+from .serializers import ProcessorSerializer
 
-# Create your views here.
+class ProcessorView(viewsets.ModelViewSet):
+    queryset = Processors.objects.all()
+    serializer_class = ProcessorSerializer 
